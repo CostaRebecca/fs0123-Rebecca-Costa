01de3c2668bf99e63eca69704b00dc777da3041c
CostaRebecca <rebeccacosta1310@gmail.com>
2023-02-10 16:54:32 +0100

    g5

---
+++ m1/s2/g5/progetto5/js/script.js
@@ -0,0 +1,17 @@
+
+
+
+function interazione(element){
+    let simbolo = element.getAttribute('data-calc')
+
+     let display = document.querySelector('#display');
+     display.value += simbolo;
+    
+ }
+
+ function totale(){
+     let display = document.querySelector('#display');
+
+     display.value = eval(display.value);
+ }
+
