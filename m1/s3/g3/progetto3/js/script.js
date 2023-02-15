b381d7c45a41a34a74cb145e06e2299db424b944
CostaRebecca <rebeccacosta1310@gmail.com>
2023-02-15 16:58:20 +0100

    new commit

---
+++ m1/s3/g3/progetto3/js/script.js
@@ -0,0 +1,33 @@
+
+let arrTask = [];
+
+
+class ListaTask{
+    constructor(_task){
+        this.nome = _task;
+        this.completed =false;
+    }
+
+     aggiungiTask(){
+        let inputTask = document.querySelector('#toDo');
+        let task = new ListaTask(inputTask.value);
+        arrTask.push(task);
+        console.log(arrTask);
+        inputTask.value = '';
+        let div = document.createElement("div");
+        div.textContent = task.nome;
+        let button = document.createElement('button'); 
+        button.textContent = 'Rimuovi Task'; 
+        button.setAttribute('onClick', 'rimuoviTask(this.parentNode)'); 
+        div.appendChild(button); 
+        document.querySelector('.task').appendChild(div);
+        
+  } 
+}
+function aggiungiTask() {
+    let attivita = new ListaTask();
+    attivita.aggiungiTask();
+}
+function rimuoviTask(div) {
+    div.remove(); 
+  }
